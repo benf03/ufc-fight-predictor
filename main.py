983eda200e328d80f22ea175c4ebab66c6fa193e
freedman-dev <208342729+benf03@users.noreply.github.com>
2025-05-23 09:13:14 -0700

    Added new feature columns and new format

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
 import pandas as pd
+from tabulate import tabulate
 
 from src.feature_engineer import compute_stat_difference, add_label_column
 
@@ -7,4 +8,10 @@ df = pd.read_csv("data/processed/testfights.csv")
 df = compute_stat_difference(df)
 df = add_label_column(df)
 
-print(df.head())
+important_cols = [
+    "fighter_a_name", "fighter_b_name",
+    "rank_diff", "last_4_win_diff", "last_4_finish_diff",
+    "win_streak_diff", "age_diff", "height_diff", "reach_diff", "winner"
+]
+# print(df.head())
+print(tabulate(df[important_cols], headers="keys", tablefmt="fancy_grid"))
